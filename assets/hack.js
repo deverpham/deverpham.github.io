8be7e4da4303cbfbcec02bc1ae3fd30fa035b860
deverpham <deverpham@gmail.com>
2017-03-16 23:45:55 +0000

    hello

--- assets/hack.js
+++ assets/hack.js
@@ -7,8 +7,9 @@
               <p className='title'>Hi, I'm Deverpham</p>
               <p className='title'> dever.stringJoin('developer','Pham')</p>
             </Header>
-            <div className='content'>
+            <div className='content flex'>
                 <Info />
+                <Activity />
             </div>
         </section>
         )
@@ -96,6 +97,128 @@ class Social extends React.Component {
             )
     }
 }
+class Activity extends React.Component {
+    render() {
+        return (
+            <section className='activity'>
+                <RecentWorks/>
+                <div className='flex autofull'>
+                    <Workexperience />
+                    <Education />
+                </div>
+            </section>
+            )
+    }
+}
+class RecentWorks  extends React.Component {
+    render() {
+        return (
+                <section className='recentwork '>
+                    <h3 className='title'>Recent works</h3>
+                    <div className='flex wrapper'>
+                        <div className='work'>
+                            <img className='work-feature' src='http://bristowdigital.com/wp-content/uploads/2016/08/iPhone-for-site-layout-1.png'/>
+                            <p className='workNane'>Weather App</p>
+                            <p className='workDay'>12/05/2017</p>
+                        </div>                  
+                        <div className='work'>
+                            <img className='work-feature' src='https://i.ytimg.com/vi/ByYE9hBfwC4/maxresdefault.jpg'/>
+                            <p className='workNane'>Sex App</p>
+                            <p className='workDay'>12/05/2017</p>
+                        </div>
+                        <div className='work'>
+                            <img className='work-feature' src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSeblkF4DAzRSeQFt-0siZVCN0o5WbnczzWyMLH6hCjlDJ1yrEFFw'/>
+                            <p className='workNane'>Shop App</p>
+                            <p className='workDay'>12/05/2017</p>
+                        </div>                        
+                        <div className='work'>
+                            <img className='work-feature' src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSpFM8gAr6Ek13MsMAdvr7EtRn_svQrb5ewVCg18KbZe6oc3o0s'/>
+                            <p className='workNane'>Cleaner App</p>
+                            <p className='workDay'>12/05/2017</p>
+                        </div>                        
+                        
+                    </div>
+                </section>
+            )
+    }
+}
+class Workexperience extends React.Component {
+    render() {
+        return (
+                <section className='workExperience'>
+                    <h3 className='title'>WORK EXPERIENCE</h3>
+                    <timeline>
+                        <field>
+                            <dot>
+                            </dot>
+                            <p className='time'>
+                            12/05/1996
+                            </p>
+                            <p className='position'>
+                                FullStack Developer
+                            </p>
+                            <p className='company'>
+                            Google INC
+                            </p>
+                        </field>
+                                                <field>
+                            <dot>
+                            </dot>
+                            <p className='time'>
+                            12/05/1996
+                            </p>
+                            <p className='position'>
+                                FullStack Developer
+                            </p>
+                            <p className='company'>
+                            Google INC
+                            </p>
+                        </field>
+                    </timeline>                    
+
+                </section>
+            )
+    }
+}
+class Education extends React.Component {
+    render() {
+        return (
+                <section className='workExperience'>
+                    <h3 className='title'>EDUCATION</h3>
+                    <timeline>
+                        <field>
+                            <dot>
+                            </dot>
+                            <p className='time'>
+                            12/05/1996
+                            </p>
+                            <p className='position'>
+                                FullStack Developer
+                            </p>
+                            <p className='company'>
+                            Google INC
+                            </p>
+                        </field>
+                                                <field>
+                            <dot>
+                            </dot>
+                            <p className='time'>
+                            12/05/1996
+                            </p>
+                            <p className='position'>
+                                FullStack Developer
+                            </p>
+                            <p className='company'>
+                            Google INC
+                            </p>
+                        </field>
+                    </timeline>                    
+
+                </section>
+            )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return  (
