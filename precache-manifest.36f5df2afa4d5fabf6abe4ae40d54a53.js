a9a3c802c2f8457b29ba7c25aa10d9510ced9dd8
THINH <deverpham@gmail.com>
2018-10-09 11:29:20 +0700

    fixed browser router

---
+++ precache-manifest.36f5df2afa4d5fabf6abe4ae40d54a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9068847911db7772808",
+    "url": "/static/js/main.f9068847.chunk.js"
+  },
+  {
+    "revision": "85310f6193f710d5485a",
+    "url": "/static/js/1.85310f61.chunk.js"
+  },
+  {
+    "revision": "f9068847911db7772808",
+    "url": "/static/css/main.3d3c02e3.chunk.css"
+  },
+  {
+    "revision": "85310f6193f710d5485a",
+    "url": "/static/css/1.d02a80eb.chunk.css"
+  },
+  {
+    "revision": "a24fb3f7eb955fb92078ecbe905fab11",
+    "url": "/index.html"
+  }
+];
