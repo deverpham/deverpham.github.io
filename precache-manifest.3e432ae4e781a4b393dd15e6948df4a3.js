a40c85262c031281ded3e46a243600974d47f36c
THINH <deverpham@gmail.com>
2018-10-13 17:31:19 +0700

    Updates

--- precache-manifest.3e432ae4e781a4b393dd15e6948df4a3.js
+++ precache-manifest.3e432ae4e781a4b393dd15e6948df4a3.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "7c0c754ff30fac27c208",
-    "url": "/static/js/main.7c0c754f.chunk.js"
+    "revision": "6baa267e40d162eac163",
+    "url": "/static/js/main.6baa267e.chunk.js"
   },
   {
     "revision": "c0afd61c49e49b3c36a5",
     "url": "/static/js/1.c0afd61c.chunk.js"
   },
   {
-    "revision": "7c0c754ff30fac27c208",
+    "revision": "6baa267e40d162eac163",
     "url": "/static/css/main.b2de33cb.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.78514bb8.chunk.css"
   },
   {
-    "revision": "ee42beb813dc6ec164376e914bba16cc",
+    "revision": "ae284e96f3fabf86436e1249b7fe4302",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b3d077a1dbea1cb7f0976459b118c97.js"
+  "/precache-manifest.3e432ae4e781a4b393dd15e6948df4a3.js"
 );
 
 workbox.clientsClaim();
